9f756366ad382030f699d375582b4d5b48ef76a3
RuzlaJabir <154322240+RuzlaJabir@users.noreply.github.com>
2024-12-25 13:28:27 +0000

    Created the app logics and layout

--- currencyapp/components/HomeScreen.js
+++ currencyapp/components/HomeScreen.js
@@ -4,7 +4,37 @@ import { TextInput, Button, Text } from 'react-native-paper';
 import DropDownPicker from "react-native-dropdown-picker";
 
 export default function HomeScreen() {
+    const [amount, setAmount] = useState("");
+    const [fromCurrency, setFromCurrency] = useState('USD');
+    const [toCurrency, setToCurrency] = useState('LKR');
+    const [result, setResult] = useState("");
+    const [error, setError] = useState("");
   
+    const [fromCurrencyOpen,setFromCurrencyOpen] = useState(false);
+    const [toCurrencyOpen,setToCurrencyOpen] = useState(false);
+  
+    const exchangeRates = {
+      USD: { LKR: 320, EUR: 0.91 },
+      LKR: { USD: 0.0031, EUR: 0.0028 },
+      EUR: { USD: 1.1, LKR: 355 },
+    };
+  
+    const handleConvert = () => {
+      if (!amount || isNaN(amount)) {
+        setError('Please enter a valid number.');
+        return;
+      }
+      setError("");
+      const rate = exchangeRates[fromCurrency][toCurrency];
+      const conResult = parseFloat(amount) * rate;
+      setResult(conResult.toFixed(2));
+    };
+  
+    const currencyOptions = [
+      { label: "USD", value: "USD"},
+      { label: "LKR", value: "LKR"},
+      { label: "EUR", value: "RUR"},
+    ];
 
   return (
     <View style={styles.container}>
